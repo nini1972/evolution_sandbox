4e2246d77d1897f833cbc95f98f0bc6fdec24187
Dominique <drey@proximus.be>
2026-06-28 21:09:33 +0200

    chore: add max_tokens limit to prevent openrouter reservation errors

--- llm_client.py
+++ llm_client.py
@@ -37,6 +37,7 @@ def generate_next_action(system_prompt: str, history: list, tools: list) -> dict
             messages=messages,
             tools=tools,
             tool_choice="auto",
+            max_tokens=2048,
         )
         # type: ignore
         message = response.choices[0].message
